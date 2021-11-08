9fbb7529cbdefbf286331bd127152cf07e869bb8
Ronan_Murphy <ronan_murphy@dell.com>
2021-11-08 15:03:42 +0000

    Adding in changes to LightController

--- src/main/java/com/murphy/homeauto/controller/LightController.java
+++ src/main/java/com/murphy/homeauto/controller/LightController.java
@@ -1,9 +1,11 @@
 package com.murphy.homeauto.controller;
 
+import com.murphy.homeauto.command.CommandManager;
 import com.murphy.homeauto.model.LightSwitch;
 import com.murphy.homeauto.repository.LightSwitchRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -15,9 +17,24 @@ public class LightController {
     @Resource
     LightSwitchRepository lightSwitchRepository;
 
+    @Resource
+    CommandManager commandManager;
+
     @GetMapping
     private Iterable<LightSwitch> getAllLightSwitches(){
         return lightSwitchRepository.findAll();
+    }
+
+    @GetMapping("turnOff")
+    public void turnOff(@RequestParam("id")Long id){
+        LightSwitch lightSwitch = lightSwitchRepository.findById(id).get();
+        commandManager.send433Code(lightSwitch.getOffCode());
+    }
+
+    @GetMapping("turnOn")
+    public void turnOn(@RequestParam("id")Long id){
+        LightSwitch lightSwitch = lightSwitchRepository.findById(id).get();
+        commandManager.send433Code(lightSwitch.getOnCode());
     }
 
 }
