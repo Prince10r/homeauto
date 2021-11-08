7e3a75f21a5f4c5d19f6b732a572435af8b4c4bd
Ronan_Murphy <ronan_murphy@dell.com>
2021-11-08 18:36:30 +0000

    Updating GateController

--- src/main/java/com/murphy/homeauto/controller/GateController.java
+++ src/main/java/com/murphy/homeauto/controller/GateController.java
@@ -3,7 +3,6 @@ package com.murphy.homeauto.controller;
 import com.murphy.homeauto.command.CommandManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -11,12 +10,12 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/gate")
 public class GateController {
-    
+
     @Resource
     CommandManager commandManager;
 
     @PostMapping("/toggle")
-    public void turnOff(@RequestParam("id")Long id){
+    public void toggleGate(){
         commandManager.toggleGate();
     }
 }
